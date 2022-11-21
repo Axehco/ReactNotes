6123186ed761fa8aeb7e490e548a3e413950e87f
Axehco <mbozhong@outlook.com>
2022-11-21 15:58:36 +0800

    057_TodoList案例_动态初始化列表

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-//创建“外壳”组件App
-import React,{Component} from 'react'
-import Hello from './components/Hello'
-import Welcome from './components/Welcome'
+import React, { Component } from 'react';
+import Header from './components/Header';
+import List from './components/List';
+import Footer from './components/Footer'
+import './App.css'
 
-//创建并暴露App组件
-export default class App extends Component{
-	render(){
-		return (
-			<div>
-				<Hello/>
-				<Welcome/>
-			</div>
-		)
-	}
+export default class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
