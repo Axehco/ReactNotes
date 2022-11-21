7ead9619353ea87ea07466b7297814cc670a7b51
Axehco <mbozhong@outlook.com>
2022-11-21 22:11:05 +0800

    062_TodoList案例_删除一个todo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import {nanoid} from 'nanoid'
 import './index.css'
 
 export default class Header extends Component {
+
+  static propTypes = {
+    addtodo: PropTypes.func.isRequired
+  }
+
+  handleKeyUp = (event) => {
+    if (event.keyCode !== 13) return
+    if (event.target.value.trim() === '') {
+      alert('输入不能为空')
+      return
+    }
+    // console.log(event.target.value)
+    this.props.addtodo({id:nanoid(), name: event.target.value, done: true})
+  }
+
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" />
+        <input onKeyUp={this.handleKeyUp} type="text" placeholder="请输入你的任务名称，按回车键确认" />
       </div>
     );
   }
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './index.css'
 
 export default class Item extends Component {
+
+  state = {mouse: false}  // 标识鼠标移入移出
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({mouse: flag})
+    }
+  }
+
+  handleCheck = (id) => {
+    return (event) => {
+      // console.log(id, event.target.checked)
+      this.props.updateTodo(id, event.target.checked)
+    }
+  }
+
   render() {
+    const {id, name, done} = this.props
     return (
       <div>
-        <li>
+        <li style={{backgroundColor:this.state.mouse ? '#ddd' : 'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
           <label>
-            <input type="checkbox" />
-            <span>xxxxx</span>
+            <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)} />
+            <span>{name}</span>
           </label>
-          <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
+          <button className="btn btn-danger" style={{ display: this.state.mouse ? 'block' : 'none'}}>删除</button>
         </li>
       </div>
     );
