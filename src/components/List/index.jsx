a5c3900ff44ba02d94dcd4e2578a364c4b03834c
Axehco <mbozhong@outlook.com>
2022-11-22 20:46:02 +0800

    071_消息订阅与发布技_pubsub

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
 import './index.css'
 
 class List extends Component {
+
+  state = { //初始化状态
+		users:[], //users初始值为数组
+		isFirst:true, //是否为第一次打开页面
+		isLoading:false,//标识是否处于加载中
+		err:'',//存储请求相关的错误信息
+	}
+
+  componentDidMount() {
+    this.token = PubSub.subscribe('getStateMsg', (_, dataObj) => {
+      this.setState(dataObj)
+    })
+  }
+
+  componentWillUnmount() {
+    PubSub.unsubscribe(this.token)
+  }
+
   render() {
-    const {users, isFirst, isLoading, err} = this.props
+    const {users, isFirst, isLoading, err} = this.state
 
     return (
       <div className="row">
-        {/* {
-          users.map((userObj) => {
-            return (
-              <div key={userObj.id} className="card">
-                <a href={userObj.html_url} target="_blank" rel="noreferrer" >
-                  <img alt='head_portrait' src={userObj.avatar_url} style={{ width: '100px' }} />
-                </a>
-                <p className="card-text">{userObj.login}</p>
-              </div>
-            )
-          })
-        } */}
         {
 					isFirst ? <h2>欢迎使用，输入关键字，随后点击搜索</h2> :
 					isLoading ? <h2>Loading......</h2> :
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
 import axios from 'axios';
-
 class Search extends Component {
 
   search = () => {
-    // console.log(this.keyWordElement.value)
-
-    // 常规解构赋值
-    // const {value} = this.keyWordElement
-    // console.log(value)
-
-    // 连起来写解构赋值
-    // const {keyWordElement} = this
-    // const {value} = keyWordElement
-    // console.log(value)
-
-    // 精简版：同时改名字
     const { keyWordElement: { value: keyword } } = this
-    // console.log(keyword)
+    console.log(keyword)
     // 发送请求前，通知App更新状态
-    this.props.updateAppState({ isFirst: false, isLoading: true })
+    PubSub.publish('getStateMsg', {isFirst: false, isLoading: true})
     axios.get(`http://localhost:3000/api1/search/users?q=${keyword}`).then(
       res => {
-        this.props.updateAppState({ isLoading: false, users: res.data.items })
+        // this.props.updateAppState({ isLoading: false, users: res.data.items })
+        PubSub.publish('getStateMsg', {isLoading: false, users: res.data.items})
       },
       err => {
-        // console.log(err)
-        this.props.updateAppState({ isLoading: false, err: err.message })
+        // this.props.updateAppState({ isLoading: false, err: err.message })
+        PubSub.publish('getStateMsg', {isLoading: false, err: err.message})
       }
     )
   }
