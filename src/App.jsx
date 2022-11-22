cf622ae53e1de8301567b5aee6a20601495d6d3d
Axehco <mbozhong@outlook.com>
2022-11-22 15:02:20 +0800

    066_脚手架配置代理_方法2

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,26 @@ import axios from 'axios'
 
 export default class App extends Component {
 
-  getStudentData = () => {
-    axios.get('http://localhost:3000/api1/student').then(
-      response => {console.log('成功了', response.data)},
-      error => {console.log('失败了', error)}
-    )
-  }
+	getStudentData = ()=>{
+		axios.get('http://localhost:3000/api1/students').then(
+			response => {console.log('成功了',response.data);},
+			error => {console.log('失败了',error);}
+		)
+	}
 
-  getCarData = () => {
-    axios.get('http://localhost:3000/api2/cars').then(
-      response => {console.log('成功了', response.data)},
-      error => {console.log('失败了', error)}
-    )
-  }
-    
-  render() {
-    return (
-      <div>
-        <button onClick={this.getStudentData}>获取学生信息</button>
-        <button onClick={this.getCarData}>获取汽车信息</button>
-      </div>
-    )
-  }
+	getCarData = ()=>{
+		axios.get('http://localhost:3000/api2/cars').then(
+			response => {console.log('成功了',response.data);},
+			error => {console.log('失败了',error);}
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.getStudentData}>点我获取学生数据</button>
+				<button onClick={this.getCarData}>点我获取汽车数据</button>
+			</div>
+		)
+	}
 }
