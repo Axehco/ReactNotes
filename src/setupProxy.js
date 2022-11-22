46353f98dee4e499b97f69bdeb95d770d7b36602
Axehco <mbozhong@outlook.com>
2022-11-22 16:39:28 +0800

    070_github搜索案例_完成案例

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,14 @@
 // const proxy = require('http-proxy-middleware')
 
-// module.exports = function (app) {
+// module.exports = function(app){
 // 	app.use(
-// 		proxy('/api1', { //遇见/api1前缀的请求，就会触发该代理配置
-// 			target: 'http://localhost:5000', //请求转发给谁
-// 			changeOrigin: true,//控制服务器收到的请求头中Host的值
-// 			pathRewrite: { '^/api1': '' } //重写请求路径(必须)
-// 		}),
-// 		proxy('/api2', {
-// 			target: 'http://localhost:5001',
-// 			changeOrigin: true,
-// 			pathRewrite: { '^/api2': '' }
-// 		}),
+// 		proxy('/api1',{ //遇见/api1前缀的请求，就会触发该代理配置
+// 			target:'http://localhost:5000', //请求转发给谁
+// 			changeOrigin:true,//控制服务器收到的请求头中Host的值
+// 			pathRewrite:{'^/api1':''} //重写请求路径(必须)
+// 		})
 // 	)
 // }
-
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
@@ -23,13 +17,6 @@ module.exports = function (app) {
 			target: 'http://localhost:5000',
 			changeOrigin: true,
 			pathRewrite: { '^/api1': '' }
-		})
-	)
-	app.use(
-		createProxyMiddleware('/api2', {
-			target: 'http://localhost:5001',
-			changeOrigin: true,
-			pathRewrite: { '^/api2': '' }
 		})
 	)
 };
