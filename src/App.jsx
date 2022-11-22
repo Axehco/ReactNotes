09bbeb6f75ee98fff54b513b4ab70b4280c66619
Axehco <mbozhong@outlook.com>
2022-11-22 22:08:44 +0800

    073_总结github搜索案例

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import List from './components/List';
-import Search from './components/Search';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<Search />
-				<List />
+				App。。。
 			</div>
 		);
 	}
