647ee6d1ad6236c93b6a32b090f4ab2f98364fab
Axehco <mbozhong@outlook.com>
2022-11-23 17:12:48 +0800

    085_嵌套路由

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import MyNavLink from '../../components/MyNavLink'
+import News from './News'
+import Meaasge from './Message'
 
 class Home extends Component {
   render() {
     return (
-      <h3>我是Home的内容</h3>
+      <div>
+        <h3>我是Home的内容</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              {/* <a className="list-group-item" href="./home-news.html">News</a> */}
+              <MyNavLink to="/home/news">News</MyNavLink>
+            </li>
+            <li>
+              {/* <a className="list-group-item active" href="./home-message.html">Message</a> */}
+              <MyNavLink to="/home/message">Message</MyNavLink>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/home/news" component={News} />
+            <Route path="/home/message" component={Meaasge} />
+            <Redirect to="/home/news" />
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
