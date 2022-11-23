7c2f291eb29964cab2bafb8342ecea7bad8e86e9
Axehco <mbozhong@outlook.com>
2022-11-23 11:30:13 +0800

    080_封装NavLink组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { NavLink, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Header from './components/Header';
+import MyNavLink from './components/MyNavLink'
 
 class App extends Component {
 	render() {
@@ -18,8 +19,10 @@ class App extends Component {
 				<div className="row">
 					<div className="col-xs-2 col-xs-offset-2">
 						<div className="list-group">
-							<NavLink activeClassName='myactive' className="list-group-item" to="/about">About</NavLink>
-							<NavLink activeClassName='myactive' className="list-group-item" to="/home">Home</NavLink>
+							{/* <NavLink activeClassName='myactive' className="list-group-item" to="/about">About</NavLink>
+							<NavLink activeClassName='myactive' className="list-group-item" to="/home">Home</NavLink> */}
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
