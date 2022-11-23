d5ed6efe7f31dc98176e596984676a09809171c5
Axehco <mbozhong@outlook.com>
2022-11-23 15:45:15 +0800

    082_解决样式丢失问题

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Header from './components/Header'
 import MyNavLink from './components/MyNavLink'
-import Test from './pages/Test'
 
 class App extends Component {
 	render() {
@@ -20,8 +19,6 @@ class App extends Component {
 				<div className="row">
 					<div className="col-xs-2 col-xs-offset-2">
 						<div className="list-group">
-							{/* <NavLink activeClassName='myactive' className="list-group-item" to="/about">About</NavLink>
-							<NavLink activeClassName='myactive' className="list-group-item" to="/home">Home</NavLink> */}
 							<MyNavLink to="/about">About</MyNavLink>
 							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
@@ -32,7 +29,6 @@ class App extends Component {
 								<Switch>
 									<Route path="/about" component={About} />
 									<Route path="/home" component={Home} />
-									<Route path="/home" component={Test} />
 								</Switch>
 							</div>
 						</div>
